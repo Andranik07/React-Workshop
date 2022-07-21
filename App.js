4c9a629ff820501829b76a305f77853f5f10aaae
Emin291 <emin.koganyan.y@tumo.org>
2022-07-21 13:20:09 +0400

    Navigation

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './Components/HomeScreen/HomeScreen';
 import SettingsScreen from './Components/SettingsScreen/SettingsScreen';
+import { Entypo } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,8 +16,26 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} options={{
+           title :'Home',
+          tabBarIcon:()=>{
+            return(
+              <Entypo name="home" size={24} color="black" />
+            );
+          },
+        }} />
+        
+      
+
+        <Tab.Screen name="Settings" component={SettingsScreen} options={{
+          title :'Settings',
+          tabBarIcon: () => {
+            return(
+              <Ionicons name="settings" size={24} color="black" />
+            );
+          },
+          tabBarActiveTintColor : 'tomato'
+        }}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
